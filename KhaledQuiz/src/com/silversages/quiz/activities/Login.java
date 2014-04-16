ca070abb48191b331e85bd777f22789042f697c3
Wahab Jawed <wahabjawed@gmail.com>
2014-04-17 00:15:20 +0500

    Google Login

--- KhaledQuiz/src/com/silversages/quiz/activities/Login.java
+++ KhaledQuiz/src/com/silversages/quiz/activities/Login.java
@@ -1,13 +1,51 @@
 package com.silversages.quiz.activities;
 
+import java.io.InputStream;
+
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import com.silversages.quiz.abstracts.QuizActivity;
 
-public class Login extends QuizActivity {
+public class Login extends QuizActivity implements ConnectionCallbacks,
+		OnConnectionFailedListener {
+
+	private static final int RC_SIGN_IN = 0;
+	// Logcat tag
+	private static final String TAG = "MainActivity";
+
+	// Profile pic image size in pixels
+	private static final int PROFILE_PIC_SIZE = 400;
+
+	// Google client to interact with Google API
+	private GoogleApiClient mGoogleApiClient;
+
+	/**
+	 * A flag indicating that a PendingIntent is in progress and prevents us
+	 * from starting further intents.
+	 */
+	private boolean mIntentInProgress;
+
+	private boolean mSignInClicked;
+
+	private ConnectionResult mConnectionResult;
 
 	ImageView image_signIn;
 	ImageView image_signUp;
@@ -20,6 +58,10 @@ public class Login extends QuizActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).addApi(Plus.API, null)
+				.addScope(Plus.SCOPE_PLUS_LOGIN).build();
 
 	}
 
@@ -62,6 +104,166 @@ public class Login extends QuizActivity {
 
 		image_facebook = (ImageView) findViewById(R.id.fb);
 
+		image_gmail.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(Login.this, "Log in", Toast.LENGTH_LONG).show();
+				signInWithGplus();
+			}
+		});
+
 	}
 
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!result.hasResolution()) {
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+					0).show();
+			return;
+		}
+
+		if (!mIntentInProgress) {
+			// Store the ConnectionResult for later usage
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				// The user has already clicked 'sign-in' so we attempt to
+				// resolve all
+				// errors until the user is signed in, or they cancel.
+				resolveSignInError();
+			}
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode,
+			Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			if (responseCode != RESULT_OK) {
+				mSignInClicked = false;
+			}
+
+			mIntentInProgress = false;
+
+			if (!mGoogleApiClient.isConnecting()) {
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	private void getProfileInformation() {
+		try {
+			if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+				Person currentPerson = Plus.PeopleApi
+						.getCurrentPerson(mGoogleApiClient);
+				String personName = currentPerson.getDisplayName();
+				String personPhotoUrl = currentPerson.getImage().getUrl();
+				String personGooglePlusProfile = currentPerson.getUrl();
+				String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+				Log.e("TAG", "Name: " + personName + ", plusProfile: "
+						+ personGooglePlusProfile + ", email: " + email
+						+ ", Image: " + personPhotoUrl);
+
+				// txtName.setText(personName);
+				// txtEmail.setText(email);
+
+				// by default the profile url gives 50x50 px image only
+				// we can replace the value with whatever dimension we want by
+				// replacing sz=X
+				personPhotoUrl = personPhotoUrl.substring(0,
+						personPhotoUrl.length() - 2)
+						+ PROFILE_PIC_SIZE;
+
+				// new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+
+			} else {
+				Toast.makeText(getApplicationContext(),
+						"Person information is null", Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+		ImageView bmImage;
+
+		public LoadProfileImage(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
+
+		protected Bitmap doInBackground(String... urls) {
+			String urldisplay = urls[0];
+			Bitmap mIcon11 = null;
+			try {
+				InputStream in = new java.net.URL(urldisplay).openStream();
+				mIcon11 = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				Log.e("Error", e.getMessage());
+				e.printStackTrace();
+			}
+			return mIcon11;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			bmImage.setImageBitmap(result);
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		mSignInClicked = false;
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+		// Get user's information
+		getProfileInformation();
+
+		// Update the UI after signin
+		// updateUI(true);
+
+	}
+
+	private void signInWithGplus() {
+		if (!mGoogleApiClient.isConnecting()) {
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+
+	/**
+	 * Method to resolve any signin errors
+	 * */
+	private void resolveSignInError() {
+		if (mConnectionResult.hasResolution()) {
+			try {
+				mIntentInProgress = true;
+				mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+			} catch (SendIntentException e) {
+				mIntentInProgress = false;
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		mGoogleApiClient.connect();
+		// updateUI(false);
+	}
+
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	protected void onStop() {
+		super.onStop();
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
 }
